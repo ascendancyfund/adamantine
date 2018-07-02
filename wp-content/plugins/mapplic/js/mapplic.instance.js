1bea2795ec9c4bdc71bb1bf867e92f70f21f760e
Diego <diego.algara@gmail.com>
2018-07-01 22:02:24 -0500

    web 1.0

---
+++ wp-content/plugins/mapplic/js/mapplic.instance.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+	var i = 1;
+
+	while (window['mapplic' + i] !== undefined) {
+		var params = window['mapplic' + i];
+		var selector = '#mapplic' + i;
+		ajaxurl = params.ajaxurl;
+
+		$(selector).mapplic({
+			'id': params.id,
+			'height': params.height
+		});
+
+		i++;
+	}
+});
